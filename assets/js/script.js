e2dca8322c8b56b44e96f091d22c5f061e9449c2
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-02-12 18:09:13 +0000

    adding in functions to our games in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Connected!")
+function runGame () {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+     
+}
+
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplayQuestion() {
+    
+}
