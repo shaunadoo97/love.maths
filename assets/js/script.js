53ecd32a567e2b4fb45b6c30853d2bcd7a418d5e
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-02-13 16:14:47 +0000

    Adding in code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function runGame () {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
@@ -19,7 +37,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion() {
-     
+
 }
 
 
@@ -28,5 +46,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplayQuestion() {
-    
+
 }
